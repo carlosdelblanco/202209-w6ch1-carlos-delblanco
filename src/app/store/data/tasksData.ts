2523cc3add483a3b7d9e0f6673db0677dff16aba
Carlos del Blanco <delblanco.carlos@gmail.com>
2022-11-01 20:52:41 +0100

    Add taskslice test

--- src/app/store/data/tasksData.ts
+++ src/app/store/data/tasksData.ts
@@ -1,7 +1,7 @@
 import { Tasks } from "../features/Task/types";
 
 const todoList: Tasks[] = [
-  { id: 1, name: "cleanup", done: true },
+  { id: 1, name: "clean", done: true },
   { id: 2, name: "fix door", done: true },
   { id: 3, name: "paint", done: false },
 ];
--- src/app/store/features/Task/taskslice.test.ts
+++ src/app/store/features/Task/taskslice.test.ts
@@ -1,13 +1,20 @@
-import tasksList from "../../data/tasksData";
-import { loadTasksActionCreator } from "./taskSlice";
+import todoList from "../../data/tasksData";
+import { loadTasksActionCreator, taskReducer } from "./taskSlice";
 import { TaskState } from "./types";
 
 describe("Given the function taskReducer", () => {
   describe("When it is invoked", () => {
     describe("And it receives three tasks", () => {
       test("Then it should return three tasks", () => {
-        const loadTasksAction = loadTasksActionCreator(tasksList);
-        const currentTaskListState: TaskState = { Tasks: [] };
+        const loadTasksAction = loadTasksActionCreator(todoList);
+        const currentTaskListState: TaskState = {
+          list: [],
+        };
+        const expectedTaskState: TaskState = {
+          list: todoList,
+        };
+        const newTaskState = taskReducer(currentTaskListState, loadTasksAction);
+        expect(newTaskState).toStrictEqual(expectedTaskState);
       });
     });
   });
